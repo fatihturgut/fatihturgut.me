126d6837689187383d7f5eb75b275ac90407f7e8
Fatih Turgut <fatih.turgut@getir.com>
2020-03-13 19:58:29 +0300

    build

---
+++ build/precache-manifest.26db5f1c31b4636858c05b72f6b3e0a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54e3566001e4cd3a9143914119dcfa9",
+    "url": "/fatihtugut.me/index.html"
+  },
+  {
+    "revision": "e6413ec8c99fe5658645",
+    "url": "/fatihtugut.me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4327a0e87554f5d89020",
+    "url": "/fatihtugut.me/static/js/2.1a350e97.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/fatihtugut.me/static/js/2.1a350e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6413ec8c99fe5658645",
+    "url": "/fatihtugut.me/static/js/main.d5165c57.chunk.js"
+  },
+  {
+    "revision": "4c831a01c650927b13ea",
+    "url": "/fatihtugut.me/static/js/runtime-main.cc78c4df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fatihtugut.me/static/media/logo.5d5d9eef.svg"
+  }
+]);
