6f396d16147bd9f2427862cd87f420e4b0418c00
Fatih Turgut <fatih.turgut@getir.com>
2020-03-13 20:37:21 +0300

    Updates

---
+++ precache-manifest.e81eb193a25efee368bfe533586169e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2846303fcb498601c48b249275d9b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f42644c6482d44e1b9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4327a0e87554f5d89020",
+    "url": "/static/js/2.1a350e97.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1a350e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f42644c6482d44e1b9",
+    "url": "/static/js/main.572bb7fc.chunk.js"
+  },
+  {
+    "revision": "9fa971d9445f057973f8",
+    "url": "/static/js/runtime-main.09541dbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.c804aebd5597133ad4ea451b9dabf0ac.js"
+  "/precache-manifest.e81eb193a25efee368bfe533586169e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
